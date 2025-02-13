90e88f99f691eb2e3b984f0746322864cc12f514
Thomaz <thomaz2pinheiro@gmail.com>
2025-02-13 15:42:58 -0300

    progress on selected text UI

--- Mobile/app/reader/[id].jsx
+++ Mobile/app/reader/[id].jsx
@@ -9,14 +9,20 @@ COISAS QUE TENHO QUE PEGAR AQUI AINDA E MUDAR NA ESTRUTURA DE DADOS DO LIVRO
 */
 
 
-import { View, Text, SafeAreaView, ActivityIndicator } from 'react-native'
+import { View, Text, SafeAreaView, ActivityIndicator, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 
 import { Reader, useReader, ReaderProvider } from '@epubjs-react-native/core';
 import { useFileSystem } from '@epubjs-react-native/expo-file-system'; // for Expo project
 
 import { Redirect, useLocalSearchParams } from 'expo-router';
+
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+
 import { useAuth } from '../hooks/AuthContext';
+
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function BookReader() {
 
@@ -30,6 +36,9 @@ export default function BookReader() {
 
     const [src, setSrc] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [selectedText, setSelectedText] = useState('');
+    const [canSelect, setCanSelect] = useState(true);
+    const [actualPage, setActualPage] = useState(0);
 
     useEffect(() => {
         const getBook = async () => {
@@ -52,7 +61,7 @@ export default function BookReader() {
         getBook();
     });
 
-    const { goToLocation } = useReader();
+    // const { goToLocation } = useReader();
 
     const [menuAparece, setMenuAparece] = useState(0);
 
@@ -68,54 +77,70 @@ export default function BookReader() {
         </View>
     )
 
+    const singleTap = Gesture.Tap()
+        .maxDuration(250)
+        .runOnJS(true)
+        .onStart(() => {
+            console.log('tap');
+            setMenuAparece((menuState) => {
+                if (menuState > 0) return 0;
+                else return 1;
+            });
+        });
+
+
     return (
         <ReaderProvider>
-            <SafeAreaView style={{ flex: 1 }}>
+            <SafeAreaView className='flex-1 relative'>
 
-                {menuAparece > 0 && <View className=''>
-                    <Text>ola mundo</Text>
+                {menuAparece == 1 && <View className='flex flex-row items-center bg-violet-400 absolute bottom-4 p-4 self-center z-10 rounded-xl'>
+                    <TouchableOpacity onPress={() => setMenuAparece(0)}>
+                        <Ionicons name="close" size={50} color="red" className='mr-6' />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setMenuAparece(2)}>
+                        <MaterialIcons name="edit-note" size={54} color="white" />
+                    </TouchableOpacity>
+                    <MaterialIcons name="drag-indicator" size={50} color="white" />
                 </View>}
 
-                <Reader
-                    src={src} // "https://s3.amazonaws.com/moby-dick/OPS/package.opf"
-                    fileSystem={useFileSystem}
-                    
-                    // Text selection feature
-                    enableSelection={true}
-                    onSelected={(selectedText) => {
-                        setMenuAparece(2)
-                        console.log('selecionou')
-                    }}
+                {menuAparece == 2 && <View className='flex items-center justify-center absolute h-screen w-screen z-10 bg-black/55'>
+                    <TouchableOpacity className='top-3 -right-1 absolute' onPress={() => setMenuAparece(0)}>
+                        <Ionicons name="close" size={50} color="red" className='mr-6' />
+                    </TouchableOpacity>
+                    <View className='p-3 flex bg-white shadow-2xl shadow-black aspect-[3/2] h-[28%] rounded-lg overflow-hidden'>
+                        <View className='border-2 rounded-md border-gray-200 bg-neutral-100 p-3'>
+                            <Text>{selectedText}</Text>
+                        </View>
+                    </View>
 
-                    onSingleTap={() => {
-                        console.log('clicou')
-                        setMenuAparece((menuState) => {
-                            if (menuState == 0) return 1;
-                            else return 0;
-                        })
-                    }}
-                    onPress={() => {
-                        console.log('clicou')
-                        setMenuAparece((menuState) => {
-                            if (menuState == 0) return 1;
-                            else return 0;
-                        })
-                    }}
-                    onDoubleTap={() => {
-                        console.log('clicou')
-                        setMenuAparece((menuState) => {
-                            if (menuState == 0) return 1;
-                            else return 0;
-                        })
-                    }}
-                    onLongPress={() => {
-                        console.log('clicou')
-                        setMenuAparece((menuState) => {
-                            if (menuState == 0) return 1;
-                            else return 0;
-                        })
-                    }}
-                />
+
+                </View>}
+
+                <GestureDetector gesture={Gesture.Exclusive(singleTap)}>
+                    <Reader
+                        src={src} // "https://s3.amazonaws.com/moby-dick/OPS/package.opf"
+                        fileSystem={useFileSystem}
+
+                        // onReady={() => {
+                        //     if (actualPage != 0) {
+                        //         console.log('OH, VAI PRA PAGINA', actualPage);
+                        //         goToLocation(actualPage['end']['cfi']);
+                        //     }
+                        // }}
+                        
+                        enableSelection={true}
+                        onSelected={(selectedText) => {
+                            setMenuAparece(1)
+                            console.log('TEXTO SELECIONADO:', selectedText);
+                            setSelectedText(selectedText.replace(/[\t\n\r\f\v]+/g, " "));
+                        }}
+
+                        onLocationChange={(totalLocations, currentLocation) => {
+                            setActualPage(currentLocation);
+                            console.log('ACTUAL PAGE:', actualPage);
+                        }}
+                    />
+                </GestureDetector>
             </SafeAreaView>
         </ReaderProvider>
     )
